17e6e49bd278c433c0ec21f188f5d9fc7d85665d
MatteoCalvanico <matteo.calvanico03@gmail.com>
2024-12-26 18:55:54 +0100

    Add Multiple Primary Keys

--- Mako/Mako/Services/Shared/Ship/Ship.cs
+++ Mako/Mako/Services/Shared/Ship/Ship.cs
@@ -1,15 +1,14 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mako.Services.Shared
 {
+    [PrimaryKey(nameof(Name), nameof(DateArrival))]
     public class Ship
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public string Name { get; set; }
-        // We should set date_arrival as primary key as well
+         public string Name { get; set; }
         public DateTime DateArrival { get; set; }
         public DateTime DateDeparture { get; set; }
         public int Pier { get; set; }
