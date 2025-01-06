ba4709883c3610df99844d32ae917a440336a4e9
zeke-code <116798879+DemaControlsUs@users.noreply.github.com>
2025-01-06 17:28:30 +0100

    chore: add initial files

---
+++ Mako/Mako.Web/Features/Requests/RequestsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Mako.Services.Shared;
+using Mako.Web.Areas;
+
+namespace Mako.Web.Features.Requests
+{
+    public partial class RequestsController : AuthenticatedBaseController
+    {
+        private readonly SharedService _sharedService;
+
+        public RequestsController(SharedService sharedService)
+        {
+            _sharedService = sharedService;
+        }
+
+        public virtual async Task<IActionResult> Index()
+        {
+            
+
+            return View("Requests");
+        }
+    }
+}
---
+++ Mako/Mako.Web/Features/Requests/RequestsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Mako.Services.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace Mako.Web.Features.Requests
+{
+    public class RequestViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime SentDate { get; set; }
+        public string Motivation { get; set; }
+        public RequestState State { get; set; }
+        public string WorkerCf { get; set; }
+        public Worker Sender { get; set; }
+    }
+
+    public enum RequestState
+    {
+        Accepted,
+        Declined,
+        Unmanaged
+    }
+
+    public class RequestsViewModel
+    {
+        public List<RequestViewModel> Requests { get; set; } = new List<RequestViewModel>();
+    }
+}
