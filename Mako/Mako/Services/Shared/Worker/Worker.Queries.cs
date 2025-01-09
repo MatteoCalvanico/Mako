b00044124ab84e526a57405c21743d055751b31d
Filippo Monti <filippo.monti15@studio.unibo.it>
2025-01-09 17:38:17 +0100

    feat: added query for data retrieval

--- Mako/Mako/Services/Shared/Worker/Worker.Queries.cs
+++ Mako/Mako/Services/Shared/Worker/Worker.Queries.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mako.Services.Shared.Enums;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Mako.Services.Shared
@@ -35,11 +39,12 @@ namespace Mako.Services.Shared
 
     public class WorkersDetailDTO
     {
-        public string Cf;
-
-        public string Name;
-
-        public string Surname;
+        public string Cf { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<string> Roles { get; set; }
+        public List<string> Certificates { get; set; }
+        public List<string> Licences { get; set; }
     }
 
 
@@ -79,7 +84,7 @@ namespace Mako.Services.Shared
         /// </summary>
         /// <param name="qry"></param>
         /// <returns></returns>
-        public async Task<WorkersDetailDTO> Query(WorkersDetailQuery qry)
+        public async Task<WorkersDetailDTO> QueryWorkerDetail(WorkersDetailQuery qry)
         {
             return await _dbContext.Workers
                 .Where(x => x.Cf == qry.Cf)
@@ -90,6 +95,158 @@ namespace Mako.Services.Shared
                     Surname = x.Surname,
                 })
                 .FirstOrDefaultAsync();
+        }
+    }
+
+
+
+    public class WorkersComplexQuery
+    {
+        public string Filter { get; set; }
+    }
+
+    public class WorkersComplexDTO
+    {
+        public IEnumerable<WorkerDTO> Workers { get; set; }
+        public int Count { get; set; }
+
+        public class WorkerDTO
+        {
+            public string Cf { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+            //liste per ruoli, certificati e licenze
+            public List<string> Roles { get; set; }
+            public List<string> Certificates { get; set; }
+            public List<string> Licences { get; set; }
+        }
+    }
+
+
+        public partial class SharedService
+    {
+        public async Task<WorkersComplexDTO> SelectWorkersComplex(WorkersComplexQuery qry)
+        {
+            var baseQuery = _dbContext.Workers.AsQueryable();
+
+            // filtro che se necessario si può aggiungere cf o id
+            if (!string.IsNullOrEmpty(qry.Filter))
+            {
+                baseQuery = baseQuery.Where(w =>
+                    w.Cf.Contains(qry.Filter)
+                    || w.Name.Contains(qry.Filter)
+                    || w.Surname.Contains(qry.Filter)
+                );
+            }
+
+
+            var resultList = await baseQuery
+                // 1) Join con WorkerRoles (si collegano Worker a WorkerRole)
+                .Join(_dbContext.WorkerRoles,
+                    worker => worker.Cf,
+                    wr => wr.WorkerCf,
+                    (worker, wr) => new { worker, wr })
+
+                //join su Role
+                .Join(_dbContext.Roles,
+                    combo => combo.wr.RoleId,
+                    r => r.Id,
+                    (combo, r) => new
+                    {
+                        combo.worker,
+                        combo.wr,
+                        role = r    // Sarà { Id, Type = RoleTypes Type }
+                    })
+
+                //join su JoinCertification
+                .Join(_dbContext.JoinCertifications,
+                    combo => combo.worker.Cf,
+                    jc => jc.WorkerCf,
+                    (combo, jc) => new
+                    {
+                        combo.worker,
+                        combo.wr,
+                        combo.role,
+                        jc
+                    })
+
+                // d) Join su Certification
+                .Join(_dbContext.Certifications,
+                     combo => combo.jc.CertificationId,
+                     cert => cert.Id,
+                     (combo, cert) => new
+                     {
+                         combo.worker,
+                         combo.wr,
+                         combo.role,
+                         combo.jc,
+                         cert
+                     })
+
+
+                //join su JoinLicence
+                .Join(_dbContext.JoinLicences,
+                    combo => combo.worker.Cf,
+                    jl => jl.WorkerCf,
+                    (combo, jl) => new
+                    {
+                        combo.worker,
+                        combo.wr,
+                        combo.role,
+                        combo.jc,
+                        combo.cert,
+                        jl
+                    })
+
+                //join su Licence
+                .Join(_dbContext.Licences,
+                     combo => combo.jl.LicenceId,
+                     licence => licence.Id,
+                     (combo, licence) => new
+                     {
+                         Cf = combo.worker.Cf,
+                         Name = combo.worker.Name,
+                         Surname = combo.worker.Surname,
+
+                         RoleType = combo.role.Type,
+                         CertType = combo.cert.Types,
+                         LicenceType = licence.Types
+                     }
+                )
+
+                .ToListAsync();
+
+            var grouped =  resultList
+                .GroupBy(x => new { x.Cf, x.Name, x.Surname })
+                .Select(grp => new WorkersComplexDTO.WorkerDTO
+                {
+                    Cf = grp.Key.Cf,
+                    Name = grp.Key.Name,
+                    Surname = grp.Key.Surname,
+                    
+                    Roles = grp
+                        .Select(x => x.RoleType.ToString())
+                        .Distinct()
+                        .ToList(),
+                    
+                    Certificates = grp
+                        .Select(x => x.CertType.ToString())
+                        .Distinct()
+                        .ToList(),
+
+                    Licences = grp
+                        .Select(x => x.LicenceType.ToString())
+                        .Distinct()
+                        .ToList()
+                })
+                .ToList();
+
+            return new WorkersComplexDTO
+            {
+                Workers = grouped,
+                Count = grouped.Count
+            };
         }
     }
 }
