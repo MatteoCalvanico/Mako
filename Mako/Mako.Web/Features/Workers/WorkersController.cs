5393b01f56e3b9b1262e6e6614f096efceb0efc5
Filippo Monti <filippo.monti15@studio.unibo.it>
2025-01-10 17:53:03 +0100

    feat: create controller, viewmodel and view

---
+++ Mako/Mako.Web/Features/Workers/WorkersController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mako.Services.Shared;
+using Mako.Web.Areas;
+using Mako.Web.Features.Workers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Mako.Web.Features.Workers
+{
+    public partial class WorkersController : AuthenticatedBaseController
+    {
+        private readonly SharedService _sharedService;
+
+        public WorkersController(SharedService sharedService)
+        {
+            _sharedService = sharedService;
+        }
+
+        public virtual async Task<IActionResult> Index()
+        {
+            // Recupera il ViewModel completo (lista di WorkerViewModel)
+            var workersViewModel = await GetAllWorkers();
+            return View("Worker", workersViewModel);
+            // Nome della view = "Worker.cshtml"
+        }
+
+        public async Task<WorkersViewModel> GetAllWorkers()
+        {
+            var viewModel = new WorkersViewModel();
+
+            try
+            {
+                // Usiamo la query complessa (WorkersComplexQuery)
+                var query = new WorkersComplexQuery
+                {
+                    Filter = null // o ciò che desideri filtrare
+                };
+
+                // Richiamiamo il tuo metodo in Worker.Queries.cs
+                // che fa la catena di join e ritorna roles, cert, licence.
+                var workersDTO = await _sharedService.SelectWorkersComplex(query);
+
+                // Mappiamo i DTO in WorkerViewModel
+                viewModel.Workers = workersDTO.Workers
+                    .Select(w => new WorkerViewModel
+                    {
+                        Cf = w.Cf,
+                        Name = w.Name,
+                        Surname = w.Surname,
+                        Roles = w.Roles,
+                        Certificates = w.Certificates,
+                        Licences = w.Licences
+                    })
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error while getting all workers", ex);
+            }
+
+            return viewModel;
+        }
+    }
+}
---
+++ Mako/Mako.Web/Features/Workers/WorkersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Mako.Web.Features.Workers
+{
+    public class WorkerViewModel
+    {
+        public string Cf { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        // Liste per ruoli, certificati e licenze
+        public List<string> Roles { get; set; }
+        public List<string> Certificates { get; set; }
+        public List<string> Licences { get; set; }
+    }
+
+    public class WorkersViewModel
+    {
+        public List<WorkerViewModel> Workers { get; set; } = new List<WorkerViewModel>();
+    }
+}
+
